bf343d75147f86c433e5a4f2ead777d9d009c006
KijoKim76 <kijokim@hotmail.com>
2023-09-14 23:58:07 +0900

    sample question changed.

--- app/frontend/Components/Examples.razor.cs
+++ app/frontend/Components/Examples.razor.cs
@@ -7,7 +7,9 @@ public sealed partial class Examples
     [Parameter, EditorRequired] public required string Message { get; set; }
     [Parameter, EditorRequired] public EventCallback<string> OnExampleClicked { get; set; }
 
-    private string WhatIsIncluded { get; } = "What is included in my Northwind Health Plus plan that is not in standard?";
+
+    private string WhatIsIncluded { get; } = "i.MX8M supports Wi-Fi? ";
+    //private string WhatIsIncluded { get; } = "What is included in my Northwind Health Plus plan that is not in standard?";
     private string WhatIsPerfReview { get; } = "What happens in a performance review?";
     private string WhatDoesPmDo { get; } = "What does a Product Manager do?";
 
